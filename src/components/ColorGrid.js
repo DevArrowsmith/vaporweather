1be57334cb31bc97d5c622fe105de91d3e5598c1
GummiCode <gummicodeblog@gmail.com>
2021-02-24 19:49:18 +0000

    Created ColorGrid component and stylesheet.

---
+++ src/components/ColorGrid.js
@@ -0,0 +1,18 @@
+import '../styles/ColorGrid.scss';
+import React from 'react';
+
+const ColorGrid = () => {
+  return (
+    <div className="color-grid">
+      <div className="block1" />
+      <div className="block2" />
+      <div className="block3" />
+      <div className="block4" />
+      <div className="block5" />
+      <div className="block6" />
+      <div className="block7" />
+    </div>
+  );
+};
+
+export default ColorGrid;
