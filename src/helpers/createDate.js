269456ce8f33be23073ee6fa30314c59a6af8039
GummiCode <gummicodeblog@gmail.com>
2021-02-24 20:30:49 +0000

    Create helper to convert unix date to string.

---
+++ src/helpers/createDate.js
@@ -0,0 +1,35 @@
+const createDate = (timestamp) => {
+  const days = [
+    "Mon",
+    "Tues",
+    "Wed",
+    "Thurs",
+    "Fri",
+    "Sat",
+    "Sun"
+  ];
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  const datestamp = new Date(["dt"] * 1000);
+  const day = days[datestamp.getDay()];
+  const date = datestamp.getDate();
+  const month = months[datestamp.getMonth()];
+
+  return `${day} ${date} ${month}`;
+};
+
+export default createDate;
