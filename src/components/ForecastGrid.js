461cc07e0ca1eed2ef0c63d59ee7860de28c754d
GummiCode <gummicodeblog@gmail.com>
2021-02-24 19:27:51 +0000

    Start ForecastGrid component with basic styling.

---
+++ src/components/ForecastGrid.js
@@ -0,0 +1,12 @@
+import '../styles/ForecastGrid.scss';
+import React from 'react';
+
+const ForecastGrid = () => {
+  return (
+    <div className="forecast-grid">
+      <p>Placeholder</p>
+    </div>
+  );
+};
+
+export default ForecastGrid;
--- src/components/ForecastWindow.js
+++ src/components/ForecastWindow.js
@@ -1,5 +1,6 @@
 import '../styles/ForecastWindow.scss'
 import React from 'react';
+import ForecastGrid from './ForecastGrid';
 
 const ForecastWindow = () => {
   return (
@@ -10,6 +11,7 @@ const ForecastWindow = () => {
           <svg xmlns="http://www.w3.org/2000/svg" width="8px" height="7px" viewBox="0 0 8 7" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2"><path d="M1 6V5h1V4h1V3h2v1h1v1h1v1h1v1H6V6H5V5H3v1H2v1H0V6h1zm0-4V1H0V0h2v1h1v1h2V1h1V0h2v1H7v1H6v1H2V2H1z"/></svg>
           </button>
       </div>
+      <ForecastGrid />
     </div>
   );
 };
