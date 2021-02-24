b6489c25b68fb9f0268d5a3c7eced8beb0d0088b
GummiCode <gummicodeblog@gmail.com>
2021-02-24 19:44:09 +0000

    Give ForecastGrid a 7 column grid.

--- src/components/ForecastGrid.js
+++ src/components/ForecastGrid.js
@@ -4,7 +4,13 @@ import React from 'react';
 const ForecastGrid = () => {
   return (
     <div className="forecast-grid">
-      <p>Placeholder</p>
+      <div className="block1" />
+      <div className="block2" />
+      <div className="block3" />
+      <div className="block4" />
+      <div className="block5" />
+      <div className="block6" />
+      <div className="block7" />
     </div>
   );
 };
